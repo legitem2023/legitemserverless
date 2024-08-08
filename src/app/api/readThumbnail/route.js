784476de91b2ebd81d3b7a34199fbae4dfde8ad8
Robert <robert@hokei.com>
2024-08-08 14:13:31 +0800

    3rd Attemp Initial

---
+++ src/app/api/readThumbnail/route.js
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '../../../../lib/prisma';
+
+export async function GET(request) {
+  try {
+    const Inventory = await prisma.inventory.findMany({
+      select: {
+        id: true,
+        name: true,
+        price: true,
+        thumbnail: true,
+        category: true,
+        color: true,
+      },
+    });
+    return new Response(JSON.stringify(Inventory), { status: 200, headers: { 'Content-Type': 'application/json' } });
+  } catch (error) {
+    console.error('Error fetching Inventory:', error);
+    return new Response(JSON.stringify({ error: 'Error fetching Inventory' }), { status: 500, headers: { 'Content-Type': 'application/json' } });
+  }
+}
