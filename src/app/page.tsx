5b773426587cbc46d54e227cd4a60e5562aedcf8
Robert Marquez <legitem2023@gmail.com>
2025-03-10 11:53:45 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
-import Thumbnail from "@/components/Products/Thumbnail";
+import SwipeTabs from "@/components/UI/SwipeTabs";
+
+function TabOneContent() {
+  return <p>This is Tab 1 content.</p>;
+}
+
+function TabTwoContent() {
+  return <p>This is Tab 2 content.</p>;
+}
+
+function TabThreeContent() {
+  return <p>This is Tab 3 content.</p>;
+}
+
 export default function Home() {
-  return (
-    <main className="flex flex-wrap justify-center items-center w-full md:w-full lg:w-[100%]">
-        <Thumbnail/>
-    </main>
-  );
+  const tabs = [
+    { label: "Tab 1", content: <TabOneContent /> },
+    { label: "Tab 2", content: <TabTwoContent /> },
+    { label: "Tab 3", content: <TabThreeContent /> },
+  ];
+
+  return <SwipeTabs tabs={tabs} />;
 }
