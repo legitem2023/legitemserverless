dba5cd2053733635a258d812c14c1f5e70506ed7
Robert Marquez <legitem2023@gmail.com>
2025-03-10 12:05:04 +0800

    Update page.tsx

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,19 +1,8 @@
 // app/protected/page.tsx
 import { useSession, signOut } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
 
 const ProtectedPage = () => {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  if (status === 'loading') {
-    return <p>Loading...</p>;
-  }
-
-  if (!session) {
-    router.push('/auth/signin');
-    return null;
-  }
+  
 
   return (
     <div className="p-4">
