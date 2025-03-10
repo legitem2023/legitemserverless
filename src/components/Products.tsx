290a572526729badf1fdad6c86720e2068530287
Robert Marquez <legitem2023@gmail.com>
2025-03-10 16:52:40 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,1 +1,9 @@
-
+import { GET_CATEGORY, GET_CHILD_INVENTORY } from './queries';
+const Products = () =>{
+const { data: ProductsData, loading: productsLoading, error: productsError } = useQuery(GET_CHILD_INVENTORY);
+if(productsLoading) return
+  return (
+    <div></div>
+  )
+}
+export default Products;
