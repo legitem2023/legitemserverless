921c156ef91fde8bc93080a08fe2463b3031381b
Robert Marquez <legitem2023@gmail.com>
2025-03-10 13:44:58 +0800

    Update SwipeTabs.tsx

--- src/components/UI/SwipeTabs.tsx
+++ src/components/UI/SwipeTabs.tsx
@@ -35,7 +35,7 @@ export default function SwipeTabs({ tabs }: SwipeTabsProps) {
         {tabs.map((tab, index) => (
           <button
             key={index}
-            className={clsx("text-[25px] flex flex-col items-center justify-center flex-1 py-2  transition-all duration-300",activeTab === index?"border-b-2 border-brown-500 text-brown-500": "text-white-500")}
+            className={clsx("text-[25px] flex flex-col items-center justify-center flex-1 py-2  transition-all duration-300",activeTab === index?"border-b-2 border-[#451b05] text-[#451b05]": "text-white-500")}
             onClick={() => handleTabClick(index)} // Fixed: Use handleTabClick
           >
             {(<Icon icon={tab.Icn}></Icon>)}
