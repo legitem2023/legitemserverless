d635f1e5379645475a7238003d1540b905568e30
Robert Marquez <legitem2023@gmail.com>
2025-03-10 17:37:43 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,16 +1,20 @@
 "use client";
-import { useQuery } from '@apollo/client';
-import { GET_CATEGORY, GET_CHILD_INVENTORY } from './queries';
-const Products = () =>{
-const { data: ProductsData, loading: productsLoading, error: productsError } = useQuery(GET_CHILD_INVENTORY);
-if(productsLoading) return
+import { useQuery } from "@apollo/client";
+import { GET_CHILD_INVENTORY } from "./queries";
+
+const Products = () => {
+  const { data: ProductsData, loading: productsLoading, error: productsError } = useQuery(GET_CHILD_INVENTORY);
+
+  if (productsLoading) return <p>Loading...</p>;
+  if (productsError) return <p>Error: {productsError.message}</p>;
+
   return (
-    <>{
-      ProductsData?.getChildInventory.map((item:any,idx:number)=>(
-        <div key={idx}></div>
-      ))
-    }
+    <>
+      {ProductsData?.getChildInventory.map((item: any, idx: number) => (
+        <div key={idx}>{/* I-add ang data dito kung gusto mo */}</div>
+      ))}
     </>
-  )
-}
+  );
+};
+
 export default Products;
