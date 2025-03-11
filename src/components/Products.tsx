8f4ea8c2e8fdb5b731f6feee7fe64b0d62d24c9f
Robert Marquez <legitem2023@gmail.com>
2025-03-11 16:49:45 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -9,15 +9,15 @@ const Products = () => {
   return (
     <div className="flex w-full">
       <div className="flex flex-col w-full shadow-md bg-[#f1f1f1] text-[#000000] rounded-xs">
-        <div className="p-2 flex flex-row">ID :</div>
-        <div className="p-2 flex flex-row">Name :</div>
-        <div className="p-2 flex flex-row">Product Type:</div>
-        <div className="p-2 flex flex-row">Brandname :</div>
-        <div className="p-2 flex flex-row">Status :</div>
-        <div className="p-2 flex flex-row">
+        <div className="p-1 flex flex-row">ID :</div>
+        <div className="p-1 flex flex-row">Name :</div>
+        <div className="p-1 flex flex-row">Product Type:</div>
+        <div className="p-1 flex flex-row">Brandname :</div>
+        <div className="p-1 flex flex-row">Status :</div>
+        <div className="p-1 flex flex-row">
           Action :
-          <Icon icon="mage:edit" className="text-[25px] m-2 text-[#ffee8f]" />
-          <Icon icon="mdi:delete" className="text-[25px] m-2 text-[#e81515]" />
+          <Icon icon="mage:edit" className="text-[25px] m-1 text-[#ffee8f]" />
+          <Icon icon="mdi:delete" className="text-[25px] m-1 text-[#e81515]" />
         </div>
       </div>
     </div>
