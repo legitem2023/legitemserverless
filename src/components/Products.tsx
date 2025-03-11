932757fc5ac1d9912092531afbc7dcaa19ae57f6
Robert Marquez <legitem2023@gmail.com>
2025-03-11 22:14:36 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,15 +6,17 @@ import Loading from "./Loading";
 const Products = () => {
   const { data, loading, error } = useQuery(GET_CHILD_INVENTORY);
  if(loading) return <Loading/>
-console.log(data);
+console.log(data.getChildInventory);
   console.log(process.env.NEXT_PUBLIC_SERVER_LINK);
   return (
-    <div className="flex flex-row w-full bg-[#f1f1f1] shadow-md">
+    <>{
+      data.getChildInventory?.map((item:any,idx:number)=>(
+      <div key={idx} className="flex flex-row w-full bg-[#f1f1f1] shadow-md">
       <div className="grow flex flex-col w-full text-[#000000] text-[15px] rounded-sm p-2">
         <div className="p-1 flex flex-row">ID :</div>
-        <div className="p-1 flex flex-row">Name :</div>
-        <div className="p-1 flex flex-row">Product Type:</div>
-        <div className="p-1 flex flex-row">Brandname :</div>
+        <div className="p-1 flex flex-row">Name : {item.name}</div>
+        <div className="p-1 flex flex-row">Product Type: {item.productType}</div>
+        <div className="p-1 flex flex-row">Brandname : {item.brandname}</div>
         <div className="p-1 flex flex-row">Department :</div>
         <div className="p-1 flex flex-row">Status :</div>
       </div>
@@ -25,7 +27,11 @@ console.log(data);
           <Icon icon="carbon:folder-details" className="text-[25px] m-1 text-[#000000]" />
         </div>
       </div>
-    </div>
+    </div>  
+      ))
+    }
+    
+    </>
   );
 };
 
