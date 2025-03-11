1be6446f17654f72e287cce1a153a0121aaa6d36
Robert Marquez <legitem2023@gmail.com>
2025-03-11 12:31:33 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false, // Disable SWC
-  experimental: {
-    workerThreads: false, // Disable multi-threading
-    cpus: 1, // Limit to 1 CPU core
-  },
   images: {
     domains: ['motortrade.com.ph'],
   },
