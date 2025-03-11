6a11de98a8f5e0acef19e955b5df482fdb066758
Robert Marquez <legitem2023@gmail.com>
2025-03-11 13:27:45 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -5,13 +5,10 @@ import { GET_CHILD_INVENTORY } from "./queries";
 const Products = () => {
   const { data, loading, error } = useQuery(GET_CHILD_INVENTORY);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return;
 
-console.log(data);
   return (
     <div className="flex w-full" >
-      <div className="flex flex-col w-full shadow-md bg-[#f1f1f1]">
+      <div className="flex flex-col w-full shadow-md bg-[#f1f1f1] text">
         <div className="p-2">ID :</div>
         <div className="p-2">Name :</div>
         <div className="p-2">Product Type:</div>
