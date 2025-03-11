7b9ba098047522db7434df76346c43c591ee0635
Robert Marquez <legitem2023@gmail.com>
2025-03-11 21:24:28 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,14 +2,14 @@
 import { useQuery } from "@apollo/client";
 import { GET_CHILD_INVENTORY } from "./graphql/queries/queries";
 import { Icon } from "@iconify/react";
-
+import Loading from "./Loading";
 const Products = () => {
   const { data, loading, error } = useQuery(GET_CHILD_INVENTORY);
-if(loading) return
-console.log(data)
+ if(loading) return <Loading/>
+console.log(data);
   return (
     <div className="flex w-full bg-[#f1f1f1] shadow-md">
-      <div className="grow flex flex-col w-full text-[#000000] text-[15px] rounded-sm p-2">
+      <div className="grow flex flex-row w-full text-[#000000] text-[15px] rounded-sm p-2">
         <div className="p-1 flex flex-row">ID :</div>
         <div className="p-1 flex flex-row">Name :</div>
         <div className="p-1 flex flex-row">Product Type:</div>
