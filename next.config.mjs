eaa5afd31d7460522a48c588a38119624a0f2123
Robert Marquez <legitem2023@gmail.com>
2025-03-11 11:49:47 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: false, // Disable SWC
-  compiler: {
-    babel: true, // Force Babel usage
-  },
   experimental: {
     workerThreads: false, // Disable multi-threading
     cpus: 1, // Limit to 1 CPU core
