6a41c7c3e3a098e4102839f5fa12f97c14dda5b1
Robert Marquez <legitem2023@gmail.com>
2025-03-11 21:46:39 +0800

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,8 @@ const Loading = () => {
   return (
     <div className="flex w-full bg-[#f1f1f1] shadow-md">
       <div className="flex flex-col w-full text-[#000000] text-[15px] rounded-sm p-2">
-        <div className="p-1 flex flex-row h-32 bg-gradient-to-r from-gray-300 via-gray-100 to-gray-300 animate-shimmer rounded-lg m-5"></div>
-        <div className="p-1 flex flex-row h-32 bg-gradient-to-r from-gray-300 via-gray-100 to-gray-300 animate-shimmer rounded-lg m-5"></div>
+        <div className="p-1 flex flex-row h-32 bg-gradient-to-r from-gray-300 via-gray-100 to-gray-300 animate-shimmer rounded-sm mb-2"></div>
+        <div className="p-1 flex flex-row h-32 bg-gradient-to-r from-gray-300 via-gray-100 to-gray-300 animate-shimmer rounded-sm mb-2"></div>
       </div> 
     </div>
   );
