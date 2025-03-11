d9607e15ac3f29143314afab7a744e3c3d0db63f
Robert Marquez <legitem2023@gmail.com>
2025-03-11 10:03:15 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,12 +3,12 @@ import { useQuery } from "@apollo/client";
 import { GET_CHILD_INVENTORY } from "./queries";
 
 const Products = () => {
-  const { data, loading, error } = useQuery(GET_CHILD_INVENTORY);
+/*  const { data, loading, error } = useQuery(GET_CHILD_INVENTORY);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-console.log(data);
+console.log(data);*/
   return (
     <div>
       
