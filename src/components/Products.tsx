a9bdccc9d1caf91473e652d5247c6e84e78d4f4c
Robert Marquez <legitem2023@gmail.com>
2025-03-11 13:54:27 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useQuery } from "@apollo/client";
 import { GET_CHILD_INVENTORY } from "./queries";
-
+import { Icon } from "@iconify/react";
 const Products = () => {
  /* const { data, loading, error } = useQuery(GET_CHILD_INVENTORY);*/
 
