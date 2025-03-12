4c7a6ff497b98afde5ec4ccca2a76643a49930a0
Robert Marquez <legitem2023@gmail.com>
2025-03-12 13:57:17 +0800

    Update Detailed.tsx

--- src/components/Detailed.tsx
+++ src/components/Detailed.tsx
@@ -20,44 +20,23 @@ const handleEdit = () => alert("Edit Clicked!");
 const handleDelete = () => alert("Delete Clicked!"); 
   return (
     <>{
-      data.getChildInventory?.map((item:any,idx:number)=>(
-      <div key={idx} className="flex flex-row w-full bg-[#f1f1f1] shadow-md mb-2">
-        <div className="grow flex flex-col w-full text-[#000000] text-[12px] rounded-sm p-2">
         
-        </div>
+      data.getChildInventory_details?.map((item:any,idx:number)=>(
+     <div key={idx} className="flex flex-row w-full bg-[#f1f1f1] shadow-md mb-2">
         <div className="grow flex flex-col w-full text-[#000000] text-[12px] rounded-sm p-2">
-        <div className="p-1 flex flex-row">
-          <div className="font-bold w-[100px]">Name</div>
-          <div>{item.name}</div>
+          <div className="p-1 flex flex-row">
+            <div className="font-bold w-[100px]">
+             <img src={item.thumbnail} className="w-[75px] h-[75px]"/>
+            </div>
+            <div className="font-bold w-[100px]" >
+            yyyy
+            </div>
+            <div className="font-bold w-[100px]" ></div>
+
+          </div>
         </div>
-        <div className="p-1 flex flex-row">
-          <div className="font-bold w-[100px]" >Type</div>
-          <div>{item.productType}</div>
-        </div>
-        <div className="p-1 flex flex-row">
-         <div className="font-bold w-[100px]">Brand</div>
-          <div>{item.brandname}</div>
-        </div>
-        <div className="p-1 flex flex-row">
-         <div className="font-bold w-[100px]">Department</div>
-         <div>For Men</div>
-        </div>
-        <div className="p-1 flex flex-row">
-          <div className="font-bold w-[100px]">Status</div>
-          <div>Active</div>
-        </div>
-      </div>
-      <div className="grow-0">
-        <div className="p-1 flex flex-col">
-          <DropdownButton
-           options={[
-             { id:item.id,label: "Edit", onClick: handleEdit },
-             { id:item.id,label: "Delete", onClick: handleDelete },
-          ]}
-          />         
-        </div>
-      </div>
-    </div>  
+        <div className="grow-0"></div>
+      </div>  
       ))
     }
     </>
