56e07715c67842b715701c753ed29acc202240d7
Robert Marquez <legitem2023@gmail.com>
2025-03-12 23:54:32 +0800

    Update mutation.ts

--- src/components/graphql/queries/mutation.ts
+++ src/components/graphql/queries/mutation.ts
@@ -1,3 +1,5 @@
+import { gql } from "@apollo/client"
+
 export const DELETE_CHILD_INVENTORY = gql`
 mutation DeleteChildInventory($deleteChildInventoryId: Int) {
   deleteChildInventory(id: $deleteChildInventoryId) {
