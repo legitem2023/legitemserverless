6e3f7c50cc7d4a197db9c4a3f083ea8fb36a7096
Robert Marquez <legitem2023@gmail.com>
2025-03-12 15:44:38 +0800

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,16 +1,22 @@
 const Loading = () => {
   return (
-    <div className="flex flex-col w-full bg-[#f1f1f1] shadow-md">
-      <div className="flex flex-col w-full text-[#000000] text-[15px] rounded-sm p-2 mb-2">
-        <div className="p-1 flex flex-row h-32 shimmer rounded-sm"></div>
-      </div> 
-      <div className="flex flex-col w-full text-[#000000] text-[15px] rounded-sm p-2 mb-2">
-        <div className="p-1 flex flex-row h-32 shimmer rounded-sm"></div>
-      </div>
-      <div className="flex flex-col w-full text-[#000000] text-[15px] rounded-sm p-2 mb-2">
+    <>
+    <div className="flex flex-col w-full bg-[#f1f1f1] shadow-md mb-2">
+      <div className="flex flex-col w-full text-[#000000] text-[15px] rounded-sm p-2">
         <div className="p-1 flex flex-row h-32 shimmer rounded-sm"></div>
       </div> 
     </div>
+    <div className="flex flex-col w-full bg-[#f1f1f1] shadow-md mb-2">
+      <div className="flex flex-col w-full text-[#000000] text-[15px] rounded-sm p-2">
+        <div className="p-1 flex flex-row h-32 shimmer rounded-sm"></div>
+      </div> 
+    </div>
+    <div className="flex flex-col w-full bg-[#f1f1f1] shadow-md mb-2">
+      <div className="flex flex-col w-full text-[#000000] text-[15px] rounded-sm p-2">
+        <div className="p-1 flex flex-row h-32 shimmer rounded-sm"></div>
+      </div> 
+    </div>
+    </>
   );
 };
 
