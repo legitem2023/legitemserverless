1ec927ef0316701f4f2c08ef0b3babb1d018d850
Robert Marquez <legitem2023@gmail.com>
2025-03-12 15:26:36 +0800

    Create ProductDetails.tsx

---
+++ src/components/ProductDetails.tsx
@@ -0,0 +1,17 @@
+import Tabs from "./Tabs";
+import Products from "./Products";
+import Detailed from "./Detailed";
+const ProductDetails = () => {
+  const tabData = [
+    { label: "Products", content: <Products/> },
+    { label: "Details", content: <Detailed/> },
+  ];
+
+  return (
+    <div className="mx-auto mt-10">
+      <Tabs tabs={tabData} />
+    </div>
+  );
+};
+
+export default ProductDetails;
