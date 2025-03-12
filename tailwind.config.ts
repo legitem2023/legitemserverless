68dffc52a69e44bd6594269ea7f4f15a23c164bb
Robert Marquez <legitem2023@gmail.com>
2025-03-12 08:35:54 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,18 @@ const config: Config = {
         'portrait': {'raw': '(orientation: portrait)'},
         'landscape': {'raw': '(orientation: landscape)'},
       },
+      keyframes: {
+        shimmer: {
+          "0%": { backgroundPosition: "-200% 0" },
+          "100%": { backgroundPosition: "200% 0" },
+        },
+      },
+      animation: {
+        shimmer: "shimmer 1.5s infinite linear",
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
