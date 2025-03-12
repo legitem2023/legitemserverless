593fae049205b1600b3f276c816661ab325b7b87
Robert Marquez <legitem2023@gmail.com>
2025-03-12 21:13:47 +0800

    Update Detailed.tsx

--- src/components/Detailed.tsx
+++ src/components/Detailed.tsx
@@ -23,7 +23,7 @@ const Detailed = () => {
         <div key={idx} className="flex flex-row w-full min-w-0 bg-[#f1f1f1] shadow-md mb-2">
           {/* Image */}
           <div className="flex-shrink-0 p-2">
-            <img src={item.thumbnail} alt={item.name} className="w-[75px] h-[75px] object-cover rounded" />
+            <img src={item.thumbnail} alt={item.name} className="w-[150px] h-[150px] object-cover rounded" />
           </div>
 
           {/* Details */}
