143dba5006dcb794612361461165ba6b6479ad06
Robert Marquez <legitem2023@gmail.com>
2025-03-12 13:28:34 +0800

    Update store.ts

--- src/components/Redux/store.ts
+++ src/components/Redux/store.ts
@@ -1,11 +1,12 @@
 'use client'
 import { configureStore } from '@reduxjs/toolkit';
 
-import cookieReducer from './cookieSlice'
- 
+import cookieReducer from './cookieSlice';
+import styleCodeReducer from './styleCodeSlice';
 const store = configureStore({
   reducer: {
     cookie:cookieReducer,
+    styleCode:styleCodeReducer
   },
 });
 
