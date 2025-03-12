59ddecf62d0b96193abe13a010ae704e300cb32d
Robert Marquez <legitem2023@gmail.com>
2025-03-12 23:23:38 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -27,17 +27,28 @@ const Products = () => {
  
   return (
     <>
-        <div className="flex flex-row w-full min-w-0 bg-[#f1f1f1] shadow-md mb-2">
-          <div className="flex-grow-3 relative p-2">
-          
-          </div>
+        <div className="flex flex-wrap w-full min-w-0 bg-[#f1f1f1] shadow-md mb-2">
           <div className="relative p-2">
-            <input type="text" name="search" list="searchList" className="p-2 text-[13px] text-[#000000]" placeholder="Search..."/>
+            <input type="text" name="search" list="searchList" className="p-2 text-[13px] text-[#000000] w-full" placeholder="Search..."/>
              <datalist id="searchList">
                <option value="A"></option> 
                <option value="B"></option>
              </datalist>          
-  </div>
+          </div>
+          <div className="flex flex-wrap flex-grow-3 relative p-2">
+            <select className="p-2 text-[13px] text-[#000000] w-full">
+              <option>Category</option>
+            </select>
+            <select className="p-2 text-[13px] text-[#000000] w-full">
+              <option>Type</option>
+            </select>
+            <select className="p-2 text-[13px] text-[#000000] w-full">
+              <option>Brand</option>
+            </select>
+            <select className="p-2 text-[13px] text-[#000000] w-full">
+              <option>Department</option>
+            </select>
+          </div>
         </div>
       {data.getChildInventory?.map((item:any,idx:number) => (
         // Added min-w-0 to product container
