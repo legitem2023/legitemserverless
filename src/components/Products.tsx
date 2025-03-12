03b4b48584b846a7a08d75948507bd55ae87400f
Robert Marquez <legitem2023@gmail.com>
2025-03-12 14:52:18 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -68,7 +68,7 @@ const handleDetail = (item:any) => {
     </div>  
       ))
     }
-    <div style={{width:useSlide?"0px":"100%"}} className="box-border flex flex-col bg-[#f1f1f1] transition ease-in-out duration-300 top-[10px] fixed">
+    <div style={{width:useSlide?"0px":"100%"}} className="box-border flex flex-col bg-[#f1f1f1] transition ease-in-out duration-300 top-[10px] fixed overflow-hidden">
       <Detailed/>
 
     </div>
