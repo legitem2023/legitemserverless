4b532c0930dccb150de18f3e0e90b737ac232006
Robert Marquez <legitem2023@gmail.com>
2025-03-12 08:47:47 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -12,6 +12,17 @@ const Products = () => {
       data.getChildInventory?.map((item:any,idx:number)=>(
       <div key={idx} className="flex flex-row w-full bg-[#f1f1f1] shadow-md mb-2">
       <div className="grow flex flex-col w-full text-[#000000] text-[12px] rounded-sm p-2">
+        <div className="p-1 flex flex-row">ID</div>
+        <div className="p-1 flex flex-row">Name</div>
+        <div className="p-1 flex flex-row">Type</div>
+        <div className="p-1 flex flex-row">Brand</div>
+        <div className="p-1 flex flex-row">Department</div>
+        <div className="p-1 flex flex-row">Status</div>
+      </div>
+
+
+        
+      <div className="grow flex flex-col w-full text-[#000000] text-[12px] rounded-sm p-2">
         <div className="p-1 flex flex-row">{item.id}</div>
         <div className="p-1 flex flex-row"><b>{item.name}</b></div>
         <div className="p-1 flex flex-row">{item.productType}</div>
