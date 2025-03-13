01e9b5343a225691936c25089b57a4ade0c54611
Robert Marquez <legitem2023@gmail.com>
2025-03-13 13:26:32 +0800

    Update ProductSwipePage.tsx

--- src/components/ProductSwipePage.tsx
+++ src/components/ProductSwipePage.tsx
@@ -11,9 +11,7 @@ export default function ProductSwipePage() {
       <SwipeContainer
         items={[
           <Products/>,
-          <div className="p-10 bg-blue-500 text-white text-center">
-          <InsertProducts/>
-          </div>,
+          <InsertProducts/>,
           <div className="p-10 bg-green-500 text-white text-center">Slide 3</div>,
         ]}
       />
