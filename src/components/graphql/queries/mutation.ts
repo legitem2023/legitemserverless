15fd3397fde42436d374131b73e0c1fbcf0f786a
Robert Marquez <legitem2023@gmail.com>
2025-03-14 00:50:07 +0800

    Update mutation.ts

--- src/components/graphql/queries/mutation.ts
+++ src/components/graphql/queries/mutation.ts
@@ -6,3 +6,12 @@ mutation DeleteChildInventory($deleteChildInventoryId: Int) {
     statusText
   }
 }`
+
+export const UPDATE_PARENT_INVENTORY = gql`
+mutation Mutation($productId: Int, $category: String, $productType: String, $brandname: String, $productName: String, $status: String) {
+  updateParentInventory(productID: $productId, category: $category, productType: $productType, brandname: $brandname, productName: $productName, status: $status) {
+    jsonToken
+    statusText
+  }
+}
+`
