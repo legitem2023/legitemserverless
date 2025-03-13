2d876d0bc025597ab9317a945833cebbcf34b354
Robert Marquez <legitem2023@gmail.com>
2025-03-13 11:40:45 +0800

    Create ProductSwipePage.tsx

---
+++ src/components/ProductSwipePage.tsx
@@ -0,0 +1,19 @@
+import SwipeContainer from "./UI/SwipeContainer";
+import { useSelector } from "react-redux";
+import Products from "./Products"
+export default function ProductSwipePage() {
+  const activeIndex = useSelector((state:any) => state.swipe.activeIndex);
+
+  return (
+    <div>
+      <h1>Active Slide: {activeIndex}</h1>
+      <SwipeContainer
+        items={[
+          <Products/>,
+          <div className="p-10 bg-blue-500 text-white text-center">Slide 2</div>,
+          <div className="p-10 bg-green-500 text-white text-center">Slide 3</div>,
+        ]}
+      />
+    </div>
+  );
+}
