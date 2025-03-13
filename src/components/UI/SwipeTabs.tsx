7992e7c59ea239aca09f043983dc502bf1d15c09
Robert Marquez <legitem2023@gmail.com>
2025-03-13 12:31:37 +0800

    Update SwipeTabs.tsx

--- src/components/UI/SwipeTabs.tsx
+++ src/components/UI/SwipeTabs.tsx
@@ -68,7 +68,7 @@ function SwipeTabsComponent({ tabs }: SwipeTabsProps) {
 
       {/* Swiper Content */}
       <Swiper
-      
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
         modules={[Pagination]}
         spaceBetween={10}
         slidesPerView={1}
