9dfd188f39b5bcccd053d5422f542df39628f5e8
Robert Marquez <legitem2023@gmail.com>
2025-03-13 15:16:33 +0800

    Update BackButton.tsx

--- src/components/UI/BackButton.tsx
+++ src/components/UI/BackButton.tsx
@@ -2,6 +2,7 @@ import { setActiveIndex as Index2 } from "../Redux/swipeSlice";
 import {useDispatch} from 'react-redux';
 const BackButton = () => {
   return (
-    <buttom onClick={()=>useDispatch(Index2(0)}>Back</button>
+    <button onClick={()=>useDispatch(Index2(0)}>Back</button>
   )
 }
+export default BackButton;
