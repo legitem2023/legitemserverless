cbf68b64e4abf4ca6392c5fe94c11a1d59a03bca
Robert Marquez <legitem2023@gmail.com>
2025-03-13 22:11:09 +0800

    Update BackButton.tsx

--- src/components/UI/BackButton.tsx
+++ src/components/UI/BackButton.tsx
@@ -7,10 +7,10 @@ import { Icon } from "@iconify/react";
 const BackButton = () => {
   const dispatch = useDispatch();
   const reset = () =>{
-    dispatch(setTypes(""));
-    dispatch(setBrands(""));
-    dispatch(setDepartments(""));
-    dispatch(setCategories(""));
+    dispatch(setTypes("Select Types"));
+    dispatch(setBrands("Select Brand"));
+    dispatch(setDepartments("Select Department"));
+    dispatch(setCategories("Select Category"));
     dispatch(Index2(0))
   }
   return (
