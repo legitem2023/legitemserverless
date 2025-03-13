242d247fc40ea073ec6b790f361ee7edda41105a
Robert Marquez <legitem2023@gmail.com>
2025-03-13 15:24:42 +0800

    Update BackButton.tsx

--- src/components/UI/BackButton.tsx
+++ src/components/UI/BackButton.tsx
@@ -1,12 +1,15 @@
 import { setActiveIndex as Index2 } from "../Redux/swipeSlice";
 import { useDispatch } from 'react-redux';
+import { Icon } from "@iconify/react";
 
 const BackButton = () => {
   const dispatch = useDispatch(); // ✅ Call useDispatch at the top level
 
   return (
     <div>
-      <button onClick={() => dispatch(Index2(0))}>Back</button> 
+      <button className="flex justify-center item-center p-1 rounded-md" onClick={() => dispatch(Index2(0))}>
+        <Icon icon="mingcute:back-fill" />Back
+      </button> 
     </div>
   );
 };
