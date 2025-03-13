6159a143657dc224b60b2ebea6bd08293cc23e96
Robert Marquez <legitem2023@gmail.com>
2025-03-13 22:37:12 +0800

    Update InsertProducts.tsx

--- src/components/UI/InsertProducts.tsx
+++ src/components/UI/InsertProducts.tsx
@@ -78,9 +78,11 @@ const InsertProducts = () => {
       <div>
        <Sortings/>
       </div>
-      <button type="submit" className="bg-blue-500 text-white px-4 py-2">
+      <div className="flex-1 flex flex-wrap relative p-2">
+      <button type="submit" className="bg-brown-500 text-white px-4 py-2">
         Submit
       </button>
+      </div>
     </form>
     </>
   );
