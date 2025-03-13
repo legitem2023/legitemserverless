757e9475e5313d8acd98ae2c9d002c9f66a11686
Robert Marquez <legitem2023@gmail.com>
2025-03-14 01:38:07 +0800

    Update EditProducts.tsx

--- src/components/UI/EditProducts.tsx
+++ src/components/UI/EditProducts.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import BackButton from "./BackButton";
 import { useSelector } from "react-redux";
 import Sortings from "./Sortings";
-import {UPDATE_PARENT_INVENTORY} from "../Redux/queries/mutation"
+import {UPDATE_PARENT_INVENTORY} from "../graphql/queries/mutation"
 
 const EditProducts = () => {
   const [UpdateParentInventory] = useMutation(UPDATE_PARENT_INVENTORY, {
