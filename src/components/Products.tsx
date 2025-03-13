1bd8257c1d4621ef1dfecc0867a94ef89b0c971b
Robert Marquez <legitem2023@gmail.com>
2025-03-13 23:05:13 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -41,7 +41,7 @@ const Products = () => {
           </div>
           <Sortings/>
           <div className="flex-1 flex relative p-2">
-            <button onClick={()=> dispatch(setActiveIndex(1))}  className="flex justify-center items-center p-2 rounded-md bg-[#451b05] text-[#ffffff] shadow-lg m-2">Add Product</button>
+            <button onClick={()=> dispatch(Index2(1))}  className="flex justify-center items-center p-2 rounded-md bg-[#451b05] text-[#ffffff] shadow-lg m-2">Add Product</button>
           </div>
         </div>
       {data.getChildInventory?.map((item:any,idx:number) => (
