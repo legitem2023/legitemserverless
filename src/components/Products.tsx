ac39a332b487b18f24db60ccf803d00e3d7bd913
Robert Marquez <legitem2023@gmail.com>
2025-03-14 00:55:09 +0800

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,7 +3,7 @@ import { useQuery } from "@apollo/client";
 import { useDispatch, useSelector } from 'react-redux';
 import {setActiveIndex} from './Redux/activeIndexSlice';
 import { setActiveIndex as Index2 } from "./Redux/swipeSlice";
-import { setTypes, setBrands, setDepartments,setCategories,setStatus,setName } from "./Redux/categorySlice";
+import { setTypes, setBrands, setDepartments,setCategories,setStatus,setName,setID} from "./Redux/categorySlice";
 
 import {setStyleCode} from './Redux/styleCodeSlice';
 import { useState } from 'react'
@@ -35,6 +35,7 @@ if (filter?.length) {
     dispatch(setCategories([filter[0].category || "Select Category"]));
     dispatch(setStatus([filter[0].status || "Select Status"]));
     dispatch(setName(filter[0].name || [""]));
+    dispatch(setID(filter[0].id || [""]));
 } else {
     dispatch(setTypes(["Select Types"]));
     dispatch(setBrands(["Select Brand"]));
@@ -42,6 +43,7 @@ if (filter?.length) {
     dispatch(setCategories(["Select Category"]));
     dispatch(setStatus(["Select Status"]));
     dispatch(setName([""]));
+    dispatch(setID([""]));
 }
   };
   const handleDelete = () => alert("Delete Clicked!");
