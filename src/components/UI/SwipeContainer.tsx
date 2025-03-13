c6823103fc2cf07e9baa84a8bb07f2307b51e4cf
Robert Marquez <legitem2023@gmail.com>
2025-03-13 11:28:44 +0800

    Create SwipeContainer.tsx

---
+++ src/components/UI/SwipeContainer.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import { useDispatch, useSelector } from "react-redux";
+import { setActiveIndex } from "../redux/swipeSlice";
+import { RootState } from "../redux/store";
+
+interface SwipeContainerProps {
+  items: React.ReactNode[];
+}
+
+const SwipeContainer: React.FC<SwipeContainerProps> = ({ items }) => {
+  const dispatch = useDispatch();
+  const activeIndex = useSelector((state: RootState) => state.swipe.activeIndex);
+
+  return (
+    <Swiper
+      modules={[Pagination]}
+      spaceBetween={20}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      onSlideChange={(swiper) => dispatch(setActiveIndex(swiper.activeIndex))}
+    >
+      {items.map((item, index) => (
+        <SwiperSlide key={index}>{item}</SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default SwipeContainer;
