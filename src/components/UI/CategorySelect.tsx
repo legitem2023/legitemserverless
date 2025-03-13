0640b8ef3b15589244302e94d76db3f49bb31f87
Robert Marquez <legitem2023@gmail.com>
2025-03-13 13:37:56 +0800

    Update CategorySelect.tsx

--- src/components/UI/CategorySelect.tsx
+++ src/components/UI/CategorySelect.tsx
@@ -1,1 +1,65 @@
+import React from 'react'
+import {GET_CATEGORY, GET_PRODUCT_TYPES, GET_BRANDS } from '../graphql/queries/queries';
+import { useSelector,useDispatch } from 'react-redux';
+import { setTypes, setBrands, setDepartments,setCategories } from "../Redux/categorySlice";
+import { useQuery } from '@apollo/client';
+import Select from './Select';
+const CategorySelect = () => {
+  const dispatch = useDispatch();
+  const selectedCategory = useSelector((state:any)=>state.category.categories);
+  const selectedType = useSelector((state:any)=>state.category.types);
+  const selectedBrand = useSelector((state:any)=>state.category.brands);
 
+  const { data:Category, loading:Category_loading } = useQuery(GET_CATEGORY);
+  const { data:Product_Type,loading:Product_loading } = useQuery(GET_PRODUCT_TYPES);
+  const { data:Brands,loading:Brand_loading } = useQuery(GET_BRANDS);
+
+  if(Category_loading) return ;
+  if(Product_loading) return ;
+  if(Brand_loading) return ;
+
+ const CollapsibleCategory = () =>{
+    return Category?.getCategory?.map((item: any) => {
+        return {
+            "Value": item.Name,
+            "Text": item.Name
+        }
+    })
+ }
+
+ const CollapsibleProductType = () =>{
+    return Product_Type?.getProductTypes?.map((item: any) => {
+        return {
+            "Value": item.Name,
+            "Text": item.Name
+        }
+    })
+ }
+
+ const CollapsibleBrandName = () =>{
+    return Brands?.getBrand?.map((item: any) => {
+        return {
+            "Value": item.Name,
+            "Text": item.Name
+        }
+    })
+ }
+
+ const CollapsiblePages = () =>{
+  const data = [{"Name":"20"},{"Name":"50"},{"Name":"100"}];
+  return data.map((item:any)=>{
+      return {
+          "Value":item.Name,
+          "Text":item.Name
+      }
+  })
+}
+
+  return (
+        <div className="flex-1 flex flex-wrap relative p-2">
+           <Select Selected={selectedCategory} InitialText="Select Category" Name="Category" Data={CollapsibleCategory()} function_event={(e:any)=>{dispatch(setCategories(e.value))}}/>
+        </div>
+  )
+}
+
+export default CategorySelect
