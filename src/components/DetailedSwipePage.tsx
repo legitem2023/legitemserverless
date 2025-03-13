b7d59a99ba8f620a545d3bb39432ef26eff506c6
Robert Marquez <legitem2023@gmail.com>
2025-03-13 23:13:43 +0800

    Create DetailedSwipePage.tsx

---
+++ src/components/DetailedSwipePage.tsx
@@ -0,0 +1,19 @@
+import SwipeContainer from "./UI/SwipeContainer";
+import { useSelector } from "react-redux";
+import Detailed from "./Detailed";
+
+export default function DetailedSwipePage() {
+  const activeIndex = useSelector((state:any) => state.swipe.activeIndex);
+
+  return (
+    <div>
+      <SwipeContainer
+        items={[
+          <Detailed/>,
+          <></>,
+          <></>,
+        ]}
+      />
+    </div>
+  );
+}
