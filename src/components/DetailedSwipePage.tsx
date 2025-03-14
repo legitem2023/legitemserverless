3ceff1508733a8cc41622fe61b4ab5d7841f1777
Robert Marquez <legitem2023@gmail.com>
2025-03-14 16:00:54 +0800

    Update DetailedSwipePage.tsx

--- src/components/DetailedSwipePage.tsx
+++ src/components/DetailedSwipePage.tsx
@@ -2,7 +2,7 @@ import SwipeContainer from "./UI/SwipeContainer";
 import { useSelector } from "react-redux";
 import Detailed from "./Detailed";
 import InsertDetails from "./InsertDetails";
-import EditDetails from ./EditDetails";
+import EditDetails from "./EditDetails";
 export default function DetailedSwipePage() {
   const activeIndex = useSelector((state:any) => state.swipe.activeIndex);
 
