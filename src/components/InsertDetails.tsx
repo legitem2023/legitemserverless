d65c470e2e61bb4d55181a73cbc97e1df00e7612
Robert Marquez <legitem2023@gmail.com>
2025-03-14 15:43:24 +0800

    Update InsertDetails.tsx

--- src/components/InsertDetails.tsx
+++ src/components/InsertDetails.tsx
@@ -125,8 +125,7 @@ export default function InsertDetails() {
       </div>
 
        <div className="flex-1 flex relative p-2">  
-        <Editor
-          apiKey="your-tinymce-api-key"
+        <Editor  apiKey='cwn0oh6tsk3swlt9qm1xpofcybpgg65bban9nsl3tdavcr12'
           onInit={(evt, editor) => (editorRef.current = editor)}
           onEditorChange={handleEditorChange}
           init={{
