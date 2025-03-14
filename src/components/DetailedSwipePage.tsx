6a336a2fcf5482fb5ed2dd9d8a0c2f8a3131b7d1
Robert Marquez <legitem2023@gmail.com>
2025-03-14 14:35:16 +0800

    Update DetailedSwipePage.tsx

--- src/components/DetailedSwipePage.tsx
+++ src/components/DetailedSwipePage.tsx
@@ -11,7 +11,7 @@ export default function DetailedSwipePage() {
         items={[
           <Detailed/>,
           <InsertDetails/>,
-          <Detailed/>,
+          <InsertDetails/>,
         ]}
       />
     </div>
