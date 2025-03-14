4b341e937fef4e58d85dcfacf96b615dfbbc89f3
Robert Marquez <legitem2023@gmail.com>
2025-03-14 16:18:30 +0800

    Update Detailed.tsx

--- src/components/Detailed.tsx
+++ src/components/Detailed.tsx
@@ -38,6 +38,9 @@ const Detailed = () => {
   }
   return (
     <div className="w-full mb-2">
+        <div className="flex flex-row w-full min-w-0 bg-[#f1f1f1] shadow-md mb-2">
+
+</div>
  {data?.getChildInventory_details?.map((item: any, idx: number) => (
         <div key={idx} className="flex flex-row w-full min-w-0 bg-[#f1f1f1] shadow-md mb-2">
           {/* Image */}
