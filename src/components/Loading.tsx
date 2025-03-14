144e51b459cf561eff9d61b2b7faceb0bef55d6d
Robert Marquez <legitem2023@gmail.com>
2025-03-14 13:35:49 +0800

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <>
-    <div className="flex flex-col w-full bg-[#f1f1f1] shadow-md mb-2">
+    <div className="flex flex-col w-full bg-[#d1d1d1] shadow-md mb-2">
       <div className="flex flex-col w-full text-[#000000] text-[15px] rounded-sm p-2">
         <div className="p-1 flex flex-row h-32 shimmer rounded-sm"></div>
       </div> 
