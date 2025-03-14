afdbdc2efa421974f7c3314ff78a7f66c251480e
Robert Marquez <legitem2023@gmail.com>
2025-03-14 20:48:23 +0800

    Update utils.ts

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -12,7 +12,7 @@ export const autoScrollTop = () => {
     const element = document.getElementById(tabId);
     if (element) {
       element.scrollTop = 0; // Scroll the element itself to top
-      element.scrollIntoView({ behavior: "smooth", block: "start" }); // Scroll the page to the element
+      
     }
   });
 };
