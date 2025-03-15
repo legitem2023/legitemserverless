e5b1ba77697d32c2b1b23c370ce809af97da7a25
Robert Marquez <legitem2023@gmail.com>
2025-03-15 17:13:51 +0800

    Update DashboardStats.tsx

--- src/components/DashboardStats.tsx
+++ src/components/DashboardStats.tsx
@@ -1,6 +1,3 @@
-// components/DashboardStats.tsx
-"use client";
-
 import { useEffect, useState } from "react";
 import StatsCard from "./StatsCard";
 
@@ -11,9 +8,7 @@ const DashboardStats = () => {
   const [totalViews, setTotalViews] = useState(0);
 
   useEffect(() => {
-    // Sample fetching function (palitan mo ng actual API call mo)
     const fetchStats = async () => {
-      // Dummy values (palitan mo ito ng actual data from GraphQL/API)
       setTodaysVisit(120);
       setTotalVisit(12345);
       setTodaysViews(200);
@@ -24,7 +19,7 @@ const DashboardStats = () => {
   }, []);
 
   return (
-    <div className="fixed bottom-4 right-4 flex flex-col gap-4">
+    <div className="flex flex-wrap justify-center md:justify-start gap-4 p-4">
       <StatsCard title="Today's Visit" value={todaysVisit} icon="mdi:account-multiple" color="bg-blue-500" />
       <StatsCard title="Total Visits" value={totalVisit} icon="mdi:chart-bar" color="bg-green-500" />
       <StatsCard title="Today's Views" value={todaysViews} icon="mdi:eye-outline" color="bg-yellow-500" />
