ae72b28850fb4a67b34136de6d05cf3f76eb3784
Robert Marquez <legitem2023@gmail.com>
2025-03-16 00:01:01 +0800

    Update NewOrder.tsx

--- src/components/NewOrder.tsx
+++ src/components/NewOrder.tsx
@@ -1,1 +1,17 @@
-rafce
+"use client";
+
+import { useState, useRef,useEffect } from "react";
+
+import { READ_ORDER } from "./graphql/queries/queries";
+export default function EditDetails() {
+ 
+
+  return (
+<>
+   <BackButton/>
+    <div className="max-w-md mx-auto shadow-md bg-[#f1f1f1]">
+          
+    </div>
+</>
+  );
+  }
