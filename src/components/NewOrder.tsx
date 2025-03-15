63a282372f8198e85901f9784184ef31afad84b7
Robert Marquez <legitem2023@gmail.com>
2025-03-16 01:06:07 +0800

    Update NewOrder.tsx

--- src/components/NewOrder.tsx
+++ src/components/NewOrder.tsx
@@ -18,19 +18,11 @@ console.log(orders.readGroupedOrderHistory);
     <div className="max-w-md mx-auto shadow-md bg-[#f1f1f1]">
           {  orders.readGroupedOrderHistory.map((item:any,idx:number) => (
 <div key={idx}>
-  <ReusableAccordion title={item.OrderNo} content={[{ label: "OrderStatus:", value: item.OrderStatus},
-                { label: "Address:", value: item.Address},
-                { label: "Contact:", value: item.Contact}].map((field, index) => (
-                <div key={index} className="flex p-[2px]">
-                  <div className="font-bold w-[70px] flex-shrink-0">
-                    {field.label}
-                  </div>
-                  <div className="truncate overflow-hidden text-ellipsis whitespace-nowrap">
-                    {field.value}
-                  </div>
-                </div>
-              ))}/>
-   
+  <ReusableAccordion title={item.OrderNo} content={(<div key={index} className="flex p-[2px]">
+<div className="font-bold w-[70px] flex-shrink-0">Status</div>
+<div className="truncate overflow-hidden text-ellipsis whitespace-nowrap">
+ {item.OrderStatus}</div>
+                </div>)}/>
 </div>
 ))  }
     </div>
