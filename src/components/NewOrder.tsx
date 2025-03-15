f6fae1e564d591e62fc1728e4febe47e9bb1de52
Robert Marquez <legitem2023@gmail.com>
2025-03-16 00:12:53 +0800

    Update NewOrder.tsx

--- src/components/NewOrder.tsx
+++ src/components/NewOrder.tsx
@@ -2,7 +2,7 @@
 import { useMutation, useQuery } from "@apollo/client";
 
 import { useState, useRef,useEffect } from "react";
-
+import BackButton from "./UI/BackButton";
 import { READ_ORDERS } from "./graphql/queries/queries";
 export default function NewOrder() {
  const { data: orders, loading: ordersLoading, error: orderError,refetch } = useQuery(READ_ORDERS, {
