fbe6258c71e77aa9a00d35bfce4097611bc4218c
Robert Marquez <legitem2023@gmail.com>
2025-03-15 21:59:40 +0800

    Update ArrowTabs.tsx

--- src/components/ArrowTabs.tsx
+++ src/components/ArrowTabs.tsx
@@ -16,7 +16,7 @@ const ArrowTabs: React.FC<ArrowTabsProps> = ({ stages, activeStage, onStageChang
         if (index === 0) {
           clipPath = "polygon(0% 0%, 100% 0%, 88% 50%, 100% 100%, 0% 100%)"; // Unang tab (flat left, arrow right)
         } else if (index === stages.length - 1) {
-          clipPath = "polygon(0% 0%, 88% 0%, 100% 50%, 88% 100%, 0% 100%, 12% 50%)"; // Huling tab (pabaliktad ang kaliwang arrow, flat right)
+          clipPath = "polygon(0% 0%, 88% 0%, 100% 50%, 88% 100%, 0% 100%, 0% 0%)"; // Huling tab (pabaliktad ang kaliwang arrow, flat right)
         }
 
         return (
