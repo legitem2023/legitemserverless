aaafc6e9237f1f1b321601f34add9ac102fd4df1
Robert Marquez <legitem2023@gmail.com>
2025-03-16 00:01:48 +0800

    Update NewOrder.tsx

--- src/components/NewOrder.tsx
+++ src/components/NewOrder.tsx
@@ -3,7 +3,7 @@
 import { useState, useRef,useEffect } from "react";
 
 import { READ_ORDERS } from "./graphql/queries/queries";
-export default function EditDetails() {
+export default function NewOrder() {
  
 
   return (
