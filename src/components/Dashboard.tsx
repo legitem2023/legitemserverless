87022a26ccbb5af2e3e9f437ec3e1b76909a5f47
Robert Marquez <legitem2023@gmail.com>
2025-03-15 19:08:14 +0800

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,23 @@
 // app/dashboard/page.tsx
 import DashboardStats from "@/components/DashboardStats";
 import SalesChart from "@/components/SalesChart";
-
+import {READ_CHART_SALES} from "@/components/graphql/queries/queries";
 const Dashboard = () => {
+
+const [usePeriod,setPeriod] = useState("Daily");
+const {data:ChartData,loading:ChartLoading} = useQuery(READ_CHART_SALES,{variables:{
+    period:usePeriod
+}});
+
+if(ChartLoading) return
+
+const SaleData = {
+    "labels":ChartData.readSales.map((data: any) => data.Interval),
+    "values":ChartData.readSales.map((data: any) => data.totalSales)
+  }
+
+console.log(SaleData);
+
   return (
     <div className="min-h-screen p-2 bg-gray-100">
                 <div className="flex-1 flex flex-wrap relative p-2 font-bold">Dashboard</div>
