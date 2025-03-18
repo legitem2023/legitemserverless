46dd380fa2768935fc4544480911bc8f90063a87
Robert Marquez <legitem2023@gmail.com>
2025-03-18 17:48:05 +0800

    Update EditDetails.tsx

--- src/components/EditDetails.tsx
+++ src/components/EditDetails.tsx
@@ -156,28 +156,35 @@ useEffect(() => {
       </div>
 
        <div className="flex-1 flex relative p-2">  
-        <Editor
-      apiKey='8uu57oloy4rinixs0tqfkwphgzagcs38v872pfyxnyt2awak'
+         <Editor
+      apiKey="8uu57oloy4rinixs0tqfkwphgzagcs38v872pfyxnyt2awak"
       init={{
         plugins: [
-          // Core editing features
-          'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-          // Your account includes a free trial of TinyMCE premium features
-          // Try the most popular premium features until Apr 1, 2025:
-          'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
+          'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 
+          'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+          'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 
+          'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 
+          'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 
+          'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 
+          'inlinecss', 'markdown', 'importword', 'exportword', 'exportpdf'
         ],
-        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        toolbar: 
+          'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | ' + 
+          'link image media table mergetags | addcomment showcomments | spellcheckdialog ' + 
+          'a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | ' + 
+          'emoticons charmap | removeformat',
         tinycomments_mode: 'embedded',
         tinycomments_author: 'Author name',
         mergetags_list: [
           { value: 'First.Name', title: 'First Name' },
           { value: 'Email', title: 'Email' },
         ],
-        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+        ai_request: (request, respondWith) => 
+          respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
       }}
       initialValue="Welcome to TinyMCE!"
     />
-
+  
       </div>
 
            <div className="flex-1 flex relative p-2"><button type="submit" className="flex justify-center items-center p-2 rounded-md bg-[#451b05] text-[#ffffff] shadow-lg m-2">Submit</button></div>
