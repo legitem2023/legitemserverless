eb9d4c95e06e24e92ebf8d2a9fd805cfa98cb644
Robert Marquez <legitem2023@gmail.com>
2025-03-18 19:17:16 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,7 +71,7 @@ const dailySalesData = {
     { label: "Transaction", Icn: "grommet-icons:transaction", content: <TransactionTab /> },
     { label: "Sales", Icn: "tdesign:money", content: <SalesTab /> },
     { label: "Statistics", Icn: "akar-icons:statistic-up", content: <StatisticsTab /> },
-{ label: "Bills", Icn: "mdi:reciept-text", content: <SettingsTab /> },
+{ label: "Bills", Icn: "mdi:receipt-text", content: <SettingsTab /> },
     { label: "Settings", Icn: "material-symbols:settings", content: <SettingsTab /> }
   ];
 
