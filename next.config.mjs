9eef472e765c7d8cceee4b3774f4f95febb8e531
Robert Marquez <legitem2023@gmail.com>
2025-03-18 18:59:31 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['motortrade.com.ph'],
-  },
+      images: {
+        domains: ['picsum.photos','tsbriguuaznlvwbnylop.supabase.co','localhost','192.168.100.86','cdn-icons-png.flaticon.com','hokei-storage.s3.ap-northeast-1.amazonaws.com','https://management-pi.vercel.app/'],
+      },
 };
 
 export default nextConfig;
