9cedb379c8b2edba6ed7ba30fb0baaf1b1d427f7
Robert Marquez <legitem2023@gmail.com>
2025-03-19 11:00:18 +0800

    Create Settings.tsx

---
+++ src/components/Settings.tsx
@@ -0,0 +1,13 @@
+const tabs = [{
+icon: "fluent:document-add-24-filled",
+content: "" },{ 
+icon: "mdi:inbox-arrow-down", 
+content: "" },{ 
+icon: "solar:settings-bold", 
+content: "" },{ 
+icon: "mdi:truck-cargo-container", 
+content: ""},{
+icon: "material-symbols:local-shipping", content: ""},{
+icon: "mdi:check-decagram", 
+content: ""},
+];
