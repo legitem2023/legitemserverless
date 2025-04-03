15efd172ead1688072c918ca1fd80ddbd3d8262f
Robert Marquez <legitem2023@gmail.com>
2025-04-03 09:51:16 +0800

    Update mutation.ts

--- src/components/graphql/queries/mutation.ts
+++ src/components/graphql/queries/mutation.ts
@@ -1,5 +1,13 @@
 import { gql } from "@apollo/client"
 
+export const SAVE_CROP_IMAGE = gql`
+mutation Mutation($saveCropImageId: String, $file: Upload) {
+  saveCropImage(id: $saveCropImageId, file: $file) {
+    jsonToken
+    statusText
+  }
+}`
+
 export const DELETE_CHILD_INVENTORY = gql`
 mutation DeleteChildInventory($deleteChildInventoryId: Int) {
   deleteChildInventory(id: $deleteChildInventoryId) {
