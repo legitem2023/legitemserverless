a1ad964fb2faa13ec28a2ebcc4dc624133b16014
Robert Marquez <legitem2023@gmail.com>
2025-06-21 01:30:04 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
   return (
     <Suspense fallback={<div>Loading....</div>}>
       <div className="flex h-[auto]">
-        <img src="/Crowd.svg" className="w-[200px] m-2 border-box" />
+        <img src="/Crowd.svg" className="w-[150px] m-2 border-box" />
       </div>
       <SwipeTabs tabs={tabs}/> {/* Default to first tab */}
     </Suspense>
