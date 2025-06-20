53301219e539f1918f72f6f8c5db268821247cbd
Robert Marquez <legitem2023@gmail.com>
2025-06-21 01:19:04 +0800

    Update SwipeTabs.tsx

--- src/components/UI/SwipeTabs.tsx
+++ src/components/UI/SwipeTabs.tsx
@@ -52,7 +52,7 @@ export default function SwipeTabs({ tabs }: SwipeTabsProps) {
   return (
     <div className="w-full flex flex-col lg:flex-row">
       {/* Tabs Header - responsive direction */}
-      <div className="bg-[#ebb4a0] border-b lg:border-b-0 lg:border-r border-gray-300 w-full">
+      <div className="bg-[#ebb4a0] border-b lg:border-b-0 lg:border-r border-gray-300 w-full border-box">
         <div className="flex lg:flex-col">
           {tabs.map((tab, index) => (
             <button
@@ -73,7 +73,7 @@ export default function SwipeTabs({ tabs }: SwipeTabsProps) {
       </div>
 
       {/* Swiper Content */}
-      <div className="w-full">
+      <div className="w-full border-box">
         <Swiper
           allowTouchMove={false}
           onSwiper={(swiper) => (swiperRef.current = swiper)}
