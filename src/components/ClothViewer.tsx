7a4c243f23fc3e0040ba02a88d6b0d569822939e
Robert Marquez <legitem2023@gmail.com>
2026-03-13 21:47:12 +0800

    Update ClothViewer.tsx

--- src/components/ClothViewer.tsx
+++ src/components/ClothViewer.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 //import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
 interface ClothViewerProps {
