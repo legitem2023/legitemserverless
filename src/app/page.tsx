ee1725e95355327d7ef6e3678ed72544fa70e2d7
Robert Marquez <legitem2023@gmail.com>
2026-03-14 08:03:17 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -650,9 +650,10 @@ export default function DeluxeAnimeGallery() {
       {/* Mobile Navigation */}
       <div className="sticky top-0 z-40 bg-[#0a0c12]/95 backdrop-blur-sm p-4 border-b border-[#e3b34c]/30">
         <div className="flex justify-between items-center">
-          <span className="text-lg font-bold bg-gradient-to-r from-white via-[#f9e6b3] to-[#e3b34c] bg-clip-text text-transparent">
+          {/*<span className="text-lg font-bold bg-gradient-to-r from-white via-[#f9e6b3] to-[#e3b34c] bg-clip-text text-transparent">
             A&R SILKSCREEN PRINTING
-          </span>
+          </span>*/}
+          <img src="/ARLogo.svg" className="h-full p-2 "/>
           <button 
             className="border border-[#e3b34c] text-[#e3b34c] text-2xl px-4 py-2 rounded-lg"
             onClick={() => setMobileMenuOpen(true)}
