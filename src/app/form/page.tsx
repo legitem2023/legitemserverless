02f1c86e1701cda4601f7d25808762130625d497
Robert Marquez <legitem2023@gmail.com>
2026-05-03 21:28:13 +0800

    Update page.tsx

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -337,7 +337,7 @@ export default function Home() {
       )}
 
       {activeTab === 'print' && (
-        <div className="print:m-0 print:p-0 flex items-center flex-col mb-3">
+        <div className="print:m-0 print:p-0 flex p-3 items-center flex-col gap-3">
           {forms.map((formSchedules, formIndex) => (
             <div
               key={formIndex}
