43c9ad66498afc2b5de3748aa85d9c6c8a406098
Robert Marquez <legitem2023@gmail.com>
2026-05-31 12:05:03 +0800

    Update page.tsx

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
+import ScanRecommendationLetter from './ScanRecommendationLetter';
 
 interface Schedule {
   date: string;
