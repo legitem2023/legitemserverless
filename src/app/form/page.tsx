2a17b48c0cf4225ff87ae1f4863eaebf77256c85
Robert Marquez <legitem2023@gmail.com>
2026-06-10 18:15:45 +0800

    Update page.tsx

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -546,8 +546,23 @@ export default function Home() {
         <div className="flex p-5 items-center justify-center">
           <ScanMasterlist
             district="Rizal"
+            category="Emergency First Responder(EFR)"
+            members={data.members.filter((data:any) => data.function==="Emergency First Responder(EFR)")}
+          />
+          <ScanMasterlist
+            district="Rizal"
             category="Communicators"
-            members={data.members}
+            members={data.members.filter((data:any) => data.function==="Communicators")}
+          />
+          <ScanMasterlist
+            district="Rizal"
+            category="Associate(Approved)"
+            members={data.members.filter((data:any) => data.function==="Associate(Approved)")}
+          />
+          <ScanMasterlist
+            district="Rizal"
+            category="Associate(Not Approved)"
+            members={data.members.filter((data:any) => data.function==="Associate(Not Approved)")}
           />
         </div>
       )}
