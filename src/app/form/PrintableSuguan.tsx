0f87716732d65239d156e0d47fd54d9f4592d6cf
Robert Marquez <legitem2023@gmail.com>
2026-06-11 20:37:18 +0800

    Update PrintableSuguan.tsx

--- src/app/form/PrintableSuguan.tsx
+++ src/app/form/PrintableSuguan.tsx
@@ -35,7 +35,7 @@ interface PrintableSuguanProps {
 
 export function PrintableSuguan({ forms, filipinoDays }: PrintableSuguanProps) {
   return (
-    <div className="flex p-5 items-center flex-col gap-3">
+    <div className="flex p-5 flex-col gap-3 print:block print:p-0 print:m-0">
       {forms.map((formSchedules, formIndex) => (
         <div
           key={formIndex}
