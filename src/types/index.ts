149a5189ea046e48adecbc25c0756d76609d572f
Robert Marquez <legitem2023@gmail.com>
2026-06-28 18:39:54 +0800

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,11 @@
 // src/types/index.ts
 
 import { PrismaClient, User, Chat, Message } from '@prisma/client';
-import { PubSub } from 'graphql-subscriptions';
 
 export interface Context {
   prisma: PrismaClient;
   userId: string | null;
-  pubsub: PubSub;
+  req: Request;
 }
 
 export interface AuthPayload {
@@ -37,58 +36,14 @@ export interface TypingStatusType {
   isTyping: boolean;
 }
 
-export interface UserWithRelations extends User {
-  chats?: ChatParticipantWithChat[];
-  sentMessages?: Message[];
-  readMessages?: MessageReadByWithMessage[];
-  deliveredMessages?: MessageDeliveredToWithMessage[];
+export interface PusherAuthResponse {
+  auth: string;
+  channelData?: string;
 }
 
-export interface ChatWithRelations extends Chat {
-  participants: ChatParticipantWithUser[];
-  messages: MessageWithRelations[];
-  creator?: User | null;
-}
-
-export interface MessageWithRelations extends Message {
-  sender: User;
-  readBy: MessageReadByWithUser[];
-  deliveredTo: MessageDeliveredToWithUser[];
-  chat: Chat;
-}
-
-export interface ChatParticipantWithUser extends ChatParticipant {
-  user: User;
-}
-
-export interface ChatParticipantWithChat extends ChatParticipant {
-  chat: Chat;
-}
-
-export interface MessageReadByWithUser extends MessageReadBy {
-  user: User;
-}
-
-export interface MessageReadByWithMessage extends MessageReadBy {
-  message: Message;
-}
-
-export interface MessageDeliveredToWithUser extends MessageDeliveredTo {
-  user: User;
-}
-
-export interface MessageDeliveredToWithMessage extends MessageDeliveredTo {
-  message: Message;
-}
-
-export interface ChatWithLastMessage extends ChatWithRelations {
-  lastMessage: MessageWithRelations | null;
-}
-
-export interface ExtendedContext extends Context {
-  req: {
-    headers: {
-      authorization?: string;
-    };
-  };
+export interface ChatWithLastMessage extends Chat {
+  lastMessage: Message | null;
+  participants: {
+    user: User;
+  }[];
 }
