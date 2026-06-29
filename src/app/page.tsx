9766ffde38af542ebd89786f9a97dbcbc328036c
Robert Marquez <legitem2023@gmail.com>
2026-06-29 16:37:45 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@
 
 import { ApolloProvider } from '@/components/providers/ApolloProvider'
 import Menu from '@/components/Menu/Menu'
+import ScanPage from '@/components/ScanPage";
 export default function Home() {
   return (
     <ApolloProvider>
       <main className="h-screen">
+        <ScanPage/>
         <Menu/>
       </main>
     </ApolloProvider>
