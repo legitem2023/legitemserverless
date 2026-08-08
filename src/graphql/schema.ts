9ed7355945ec788e482a1cdcd950dc45476c4100
Robert Marquez <legitem2023@gmail.com>
2026-08-08 18:30:27 +0800

    Add Member and Schedule types to GraphQL schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -11,6 +11,7 @@ export const typeDefs = gql`
     lastSeen: String!
     createdAt: String!
     updatedAt: String!
+    member: Member  # Added: Link to member profile
   }
 
   enum UserStatus {
@@ -98,7 +99,113 @@ export const typeDefs = gql`
     duration: Int
   }
 
+  # ============ MEMBER TYPES ============
+  type Member {
+    id: ID!
+    userId: String!
+    name: String!
+    kapisanan: Kapisanan!
+    kahilingan: Boolean!
+    callSign: String
+    function: MemberFunction!
+    picture: String
+    Petsa_ng_maging_scan: String
+    Pagpapatibay: String
+    AssociateCategory: String
+    AmatureCallsign: String
+    user: User!
+    schedules: [Schedule!]!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type Schedule {
+    id: ID!
+    memberId: String!
+    service: ServiceType!
+    date: String!
+    day: String!
+    time: String!
+    member: Member!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  enum Kapisanan {
+    BUKLOD
+    KADIWA
+    BINHI
+  }
+
+  enum MemberFunction {
+    ASSOCIATE_MEMBERS_APPROVED
+    ASSOCIATE_MEMBERS_NOT_APPROVED
+    EMERGENCY_FIRST_RESPONDER
+  }
+
+  enum ServiceType {
+    WORSHIP
+    PNK
+    DISTRITO
+  }
+
+  # Member Input Types
+  input CreateMemberInput {
+    userId: String!
+    name: String!
+    kapisanan: Kapisanan!
+    kahilingan: Boolean
+    callSign: String
+    function: MemberFunction!
+    picture: String
+    Petsa_ng_maging_scan: String
+    Pagpapatibay: String
+    AssociateCategory: String
+    AmatureCallsign: String
+  }
+
+  input UpdateMemberInput {
+    name: String
+    kapisanan: Kapisanan
+    kahilingan: Boolean
+    callSign: String
+    function: MemberFunction
+    picture: String
+    Petsa_ng_maging_scan: String
+    Pagpapatibay: String
+    AssociateCategory: String
+    AmatureCallsign: String
+  }
+
+  input CreateScheduleInput {
+    memberId: String!
+    service: ServiceType!
+    date: String!
+    day: String!
+    time: String!
+  }
+
+  input UpdateScheduleInput {
+    service: ServiceType
+    date: String
+    day: String
+    time: String
+  }
+
+  input MemberFilterInput {
+    kapisanan: Kapisanan
+    function: MemberFunction
+    kahilingan: Boolean
+    search: String
+  }
+
+  type MemberWithSchedules {
+    member: Member!
+    schedules: [Schedule!]!
+  }
+
   type Query {
+    # Existing queries
     me: User!
     user(id: ID!): User
     users(search: String): [User!]!
@@ -107,9 +214,28 @@ export const typeDefs = gql`
     chatMessages(chatId: ID!, limit: Int, offset: Int): [Message!]!
     message(id: ID!): Message
     searchChats(query: String!): [Chat!]!
+
+    # Member queries
+    getMember(id: ID!): Member
+    getMemberByUserId(userId: String!): Member
+    getMembers(filter: MemberFilterInput, limit: Int, offset: Int): [Member!]!
+    getMembersByKapisanan(kapisanan: Kapisanan!): [Member!]!
+    getMembersByFunction(function: MemberFunction!): [Member!]!
+    getMembersWithSchedules(date: String): [Member!]!
+    getMembersWithPendingRequests: [Member!]!
+    
+    # Schedule queries
+    getSchedule(id: ID!): Schedule
+    getSchedulesByMember(memberId: String!): [Schedule!]!
+    getSchedulesByDate(date: String!): [Schedule!]!
+    getSchedulesByDateRange(startDate: String!, endDate: String!): [Schedule!]!
+    getSchedulesByService(service: ServiceType!): [Schedule!]!
+    getMemberScheduleConflict(memberId: String!, date: String!, time: String!): Boolean!
+    getSchedulesWithMembers(date: String): [MemberWithSchedules!]!
   }
 
   type Mutation {
+    # Existing mutations
     register(username: String!, email: String!, password: String!): AuthPayload!
     login(email: String!, password: String!): AuthPayload!
     updateProfile(username: String, profilePicture: String): User!
@@ -137,5 +263,21 @@ export const typeDefs = gql`
     stopTyping(chatId: ID!): TypingStatus!
     
     authenticatePusher(socketId: String!, channelName: String!): PusherAuth!
+
+    # Member mutations
+    createMember(input: CreateMemberInput!): Member!
+    updateMember(id: ID!, input: UpdateMemberInput!): Member!
+    deleteMember(id: ID!): Boolean!
+    updateMemberKapisanan(id: ID!, kapisanan: Kapisanan!): Member!
+    updateMemberFunction(id: ID!, function: MemberFunction!): Member!
+    toggleMemberKahilingan(id: ID!): Member!
+    
+    # Schedule mutations
+    createSchedule(input: CreateScheduleInput!): Schedule!
+    createMultipleSchedules(inputs: [CreateScheduleInput!]!): [Schedule!]!
+    updateSchedule(id: ID!, input: UpdateScheduleInput!): Schedule!
+    deleteSchedule(id: ID!): Boolean!
+    deleteAllSchedulesByMember(memberId: String!): Boolean!
+    clearScheduleConflicts(memberId: String!, date: String!): [Schedule!]!
   }
 `;
